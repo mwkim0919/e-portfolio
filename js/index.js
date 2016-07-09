e939911eb2c3852b0edd206db1776305e2dc5b5d
Minwoo Kim <mwkim0919@gmail.com>
2016-07-08 17:26:30 -0700

    education section done. scroll fadein event added.

--- js/index.js
+++ js/index.js
@@ -29,7 +29,23 @@ function scrollToID(id, speed){
 	}
 }
 if (typeof console === "undefined") {
-    console = {
-        log: function() { }
-    };
+	console = {
+		log: function() { }
+	};
 }
+
+$(function() {
+	$(window).scroll( function(){
+		$('.scrollfade').each( function(i){
+			var bottom_of_object = $(this).position().top + $(this).outerHeight();
+			var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+			/* Adjust the "200" to either have a delay or that the content starts fading a bit before you reach it  */
+			bottom_of_window = bottom_of_window + 200;  
+
+			if( bottom_of_window > bottom_of_object ){
+				$(this).animate({'opacity':'1'},500);
+			}
+		}); 
+	});
+});
